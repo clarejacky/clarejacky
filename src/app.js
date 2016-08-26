cb47202a392fc33fb1c18b9f8eeae28e6a9e7a67
Clare Jacky <clare.jacky@ave81.com>
2016-08-26 09:10:50 -0500

    styling changes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'YRR' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'inclusion & you' },
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'YOU' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'inclusion' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'about' }
     ]);
 
