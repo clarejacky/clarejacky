6c0b209f74b457f11a16642a9875024ee19e8a41
Clare Jacky <clare.jacky@ave81.com>
2016-11-14 11:18:53 -0600

    nav changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'YOU';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'YOU' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'inclusion' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'about' }
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'D&I' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'TECH' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'MORE ABOUT ME' }
     ]);
 
     this.router = router;
